89f64bc759759d2117c54e6c70fb185a98e2b6b2
Ilmaan <ilmaanzia@gmail.com>
2020-11-12 01:01:11 +0530

    custom

--- turt.py
+++ turt.py
@@ -2,18 +2,21 @@ import turtle as t
 import time as ti
 from itertools import cycle
 
-colors = cycle(['red','white','yellow','green','blue','grey','purple','pink','brown','orange'])
+colors = cycle(['red','white','yellow','green','blue','grey','purple','pink','brown','orange','violet'])
 
-def circle(size,angle,forw,shape):
+def circle(size,angle,forw):
     t.pencolor(next(colors))
     t.circle(size)
     t.right(angle)
     t.forward(forw)
-    circle(size+3,angle+1,forw+5)
+    circle(size+1,angle+1,forw+5)
 
+s = int(input("enter size"))
+a = int(input("enter angle"))
+f = int(input("enter distance"))
 t.bgcolor("black")
-t.speed(12)
+t.speed(9)
 t.pensize(2)
-circle(30,10,1)
+circle(s,180,1)
 ti.sleep(5)
 t.hideturtle()
